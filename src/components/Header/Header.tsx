f301b67f7c75dcd33b9b43e9f00ff624cdf01bf0
Ivan <offlane@tut.by>
2023-02-22 01:34:06 +0300

    header layout, fixed tailwind config

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,57 @@
 import React from 'react';
+import styles from './Header.module.scss';
+import logo from '../../assets/images/eco-logo.png';
+import userIcon from '../../assets/images/user-icon.png';
+import { NavLink } from 'react-router-dom';
+import { RiShoppingBagLine, RiHeartLine, RiMenuLine } from 'react-icons/ri';
+
+const navLinks = [
+  { path: 'home', display: 'Home' },
+  { path: 'shop', display: 'Shop' },
+  { path: 'cart', display: 'Cart' },
+];
 
 export const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className={styles.header}>
+      <div className={styles.navWrapper}>
+        <div className={styles.logo}>
+          <img src={logo} alt={logo} />
+          <div>
+            <h1>Multimart</h1>
+            <p>Since 1995</p>
+          </div>
+        </div>
+        <div className={styles.navigation}>
+          <ul className={styles.menu}>
+            {navLinks.map((i) => (
+              <li className={styles.navItem} key={i.path}>
+                <NavLink
+                  to={i.path}
+                  className={(navClass) => (navClass.isActive ? styles.navActive : '')}>
+                  {i.display}
+                </NavLink>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className={styles.navIcons}>
+          <span className={styles.favIcon}>
+            <RiHeartLine />
+          </span>
+          <span className={styles.cartIcon}>
+            <RiShoppingBagLine />
+          </span>
+          <span>
+            <img src={userIcon} alt={userIcon} />
+          </span>
+        </div>
+        <div className={styles.mobileMenu}>
+          <span>
+            <RiMenuLine />
+          </span>
+        </div>
+      </div>
+    </header>
+  );
 };
--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -5,12 +5,12 @@ import { Header } from '../Header/Header';
 
 export const Layout = () => {
   return (
-    <>
+    <div className="container mx-auto px-4">
       <Header />
       <div>
         <Routers />
       </div>
       <Footer />
-    </>
+    </div>
   );
 };
