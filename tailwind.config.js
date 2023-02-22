f87dc5d60f85f0713ba6bb3912e67ae1eaafa679
Ivan <offlane@tut.by>
2023-02-23 01:11:55 +0300

    changed index.scss & tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,19 @@ module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   important: true,
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: {
+        bgcard: {
+          '01': '#fdefe6',
+          '02': '#d6e5fb',
+          '03': '#ceebe9',
+          '04': '#e2f2b2',
+        },
+        bgprod: {
+          '01': '#d6e5fb',
+        },
+      },
+    },
   },
   plugins: [],
 };
